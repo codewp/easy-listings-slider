ec4a2e96866702dd0d501621feabfe99f22bb78f
taher <tah3r.a@gmail.com>
2015-06-22 18:31:36 +0430

    first commit

---
+++ includes/class-els-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Easy_Listings_Slider
+ * @subpackage Easy_Listings_Slider/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Easy_Listings_Slider
+ * @subpackage Easy_Listings_Slider/includes
+ * @author     taher.atashbar@gmail.com
+ */
+class ELS_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-els-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Easy_Listings_Slider
+ * @subpackage Easy_Listings_Slider/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Easy_Listings_Slider
+ * @subpackage Easy_Listings_Slider/includes
+ * @author     taher.atashbar@gmail.com
+ */
+class ELS_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
