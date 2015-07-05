112bd80cd7350e81d040329305d817d4325a2a51
taher <tah3r.a@gmail.com>
2015-07-05 19:34:29 +0430

    Adding a feature for adding shortcodes to the plugin loader.

--- includes/class-els-loader.php
+++ includes/class-els-loader.php
@@ -19,7 +19,7 @@
  *
  * @package    Easy_Listings_Slider
  * @subpackage Easy_Listings_Slider/includes
- * @author     taher.atashbar@gmail.com
+ * @author     Taher Atashbar <taher.atashbar@gmail.com>
  */
 class ELS_Loader {
 
@@ -42,6 +42,15 @@ class ELS_Loader {
 	protected $filters;
 
 	/**
+	 * The array of shortcodes.
+	 *
+	 * @since   1.0.0
+	 * @access  protected
+	 * @var     array   $shortcodes	  The shortcodes of the plugin.
+	 */
+	protected $shortcodes;
+
+	/**
 	 * Initialize the collections used to maintain the actions and filters.
 	 *
 	 * @since    1.0.0
@@ -57,11 +66,11 @@ class ELS_Loader {
 	 * Add a new action to the collection to be registered with WordPress.
 	 *
 	 * @since    1.0.0
-	 * @param      string               $hook             The name of the WordPress action that is being registered.
-	 * @param      object               $component        A reference to the instance of the object on which the action is defined.
-	 * @param      string               $callback         The name of the function definition on the $component.
-	 * @param      int      Optional    $priority         The priority at which the function should be fired.
-	 * @param      int      Optional    $accepted_args    The number of arguments that should be passed to the $callback.
+	 * @param    string               $hook             The name of the WordPress action that is being registered.
+	 * @param    object               $component        A reference to the instance of the object on which the action is defined.
+	 * @param    string               $callback         The name of the function definition on the $component.
+	 * @param    int      Optional    $priority         The priority at which the function should be fired.
+	 * @param    int      Optional    $accepted_args    The number of arguments that should be passed to the $callback.
 	 */
 	public function add_action( $hook, $component, $callback, $priority = 10, $accepted_args = 1 ) {
 		$this->actions = $this->add( $this->actions, $hook, $component, $callback, $priority, $accepted_args );
@@ -71,14 +80,26 @@ class ELS_Loader {
 	 * Add a new filter to the collection to be registered with WordPress.
 	 *
 	 * @since    1.0.0
-	 * @param      string               $hook             The name of the WordPress filter that is being registered.
-	 * @param      object               $component        A reference to the instance of the object on which the filter is defined.
-	 * @param      string               $callback         The name of the function definition on the $component.
-	 * @param      int      Optional    $priority         The priority at which the function should be fired.
-	 * @param      int      Optional    $accepted_args    The number of arguments that should be passed to the $callback.
+	 * @param    string               $hook             The name of the WordPress filter that is being registered.
+	 * @param    object               $component        A reference to the instance of the object on which the filter is defined.
+	 * @param    string               $callback         The name of the function definition on the $component.
+	 * @param    int      Optional    $priority         The priority at which the function should be fired.
+	 * @param    int      Optional    $accepted_args    The number of arguments that should be passed to the $callback.
 	 */
 	public function add_filter( $hook, $component, $callback, $priority = 10, $accepted_args = 1 ) {
 		$this->filters = $this->add( $this->filters, $hook, $component, $callback, $priority, $accepted_args );
+	}
+
+	/**
+	 * Add a new shortcode to the collection to be registered with WordPress
+	 *
+	 * @since    1.0.0
+	 * @param    string        $tag           The name of the new shortcode.
+	 * @param    object        $component      A reference to the instance of the object on which the shortcode is defined.
+	 * @param    string        $callback       The name of the function that defines the shortcode.
+	 */
+	public function add_shortcode( $tag, $component, $callback ) {
+		$this->shortcodes = $this->add( $this->shortcodes, $tag, $component, $callback, 0, 0 );
 	}
 
 	/**
@@ -87,12 +108,12 @@ class ELS_Loader {
 	 *
 	 * @since    1.0.0
 	 * @access   private
-	 * @param      array                $hooks            The collection of hooks that is being registered (that is, actions or filters).
-	 * @param      string               $hook             The name of the WordPress filter that is being registered.
-	 * @param      object               $component        A reference to the instance of the object on which the filter is defined.
-	 * @param      string               $callback         The name of the function definition on the $component.
-	 * @param      int      Optional    $priority         The priority at which the function should be fired.
-	 * @param      int      Optional    $accepted_args    The number of arguments that should be passed to the $callback.
+	 * @param    array                $hooks            The collection of hooks that is being registered (that is, actions or filters).
+	 * @param    string               $hook             The name of the WordPress filter that is being registered.
+	 * @param    object               $component        A reference to the instance of the object on which the filter is defined.
+	 * @param    string               $callback         The name of the function definition on the $component.
+	 * @param    int      Optional    $priority         The priority at which the function should be fired.
+	 * @param    int      Optional    $accepted_args    The number of arguments that should be passed to the $callback.
 	 * @return   type                                   The collection of actions and filters registered with WordPress.
 	 */
 	private function add( $hooks, $hook, $component, $callback, $priority, $accepted_args ) {
@@ -122,6 +143,10 @@ class ELS_Loader {
 
 		foreach ( $this->actions as $hook ) {
 			add_action( $hook['hook'], array( $hook['component'], $hook['callback'] ), $hook['priority'], $hook['accepted_args'] );
+		}
+
+		foreach ( $this->shortcodes as $hook ) {
+			add_shortcode( $hook['hook'], array( $hook['component'], $hook['callback'] ) );
 		}
 
 	}
