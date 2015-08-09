cffc6e2115d9854fc3a8dfbb7e76743e441feb54
taher <tah3r.a@gmail.com>
2015-08-09 18:02:56 +0430

    Saving font-weight option of caption styles.

--- admin/metaboxes/class-els-meta-box-slider-captions.php
+++ admin/metaboxes/class-els-meta-box-slider-captions.php
@@ -97,6 +97,7 @@ class ELS_Meta_Box_Slider_Captions extends ELS_Admin_Controller {
 				$sanitized_caption['height']           = absint( $caption['height'] );
 				$sanitized_caption['font_size']        = absint( $caption['font_size'] ) > 0 ? absint( $caption['font_size'] ) : 20;
 				$sanitized_caption['font_family']	   = sanitize_text_field( $caption['font_family'] );
+				$sanitized_caption['font_weight']	   = in_array( $caption['font_weight'], array( 'normal', 100, 200, 300, 400, 500, 600, 700, 800, 900 ) ) ? $caption['font_weight'] : 'normal';
 				$sanitized_caption['text_align']       = in_array( $caption['text_align'], array( 'left', 'center', 'right' ) ) ? $caption['text_align'] : 'center';
 				$sanitized_caption['color']            = $validator->validate_color( $caption['color'] ) ? $caption['color'] : '#000000';
 				$sanitized_caption['background_color'] = $validator->validate_color( $caption['background_color'] ) ? $caption['background_color'] : '';
