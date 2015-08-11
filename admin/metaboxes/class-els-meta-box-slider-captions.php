150af63058268c080e5f072de466fab96da7660d
taher <tah3r.a@gmail.com>
2015-08-11 17:15:37 +0430

    Saving caption padding style field.

--- admin/metaboxes/class-els-meta-box-slider-captions.php
+++ admin/metaboxes/class-els-meta-box-slider-captions.php
@@ -95,6 +95,7 @@ class ELS_Meta_Box_Slider_Captions extends ELS_Admin_Controller {
 				$sanitized_caption['offsety']          = (int) $caption['offsety'];
 				$sanitized_caption['width']            = absint( $caption['width'] );
 				$sanitized_caption['height']           = absint( $caption['height'] );
+				$sanitized_caption['padding']          = absint( $caption['padding'] );
 				$sanitized_caption['font_size']        = absint( $caption['font_size'] ) > 0 ? absint( $caption['font_size'] ) : 20;
 				$sanitized_caption['font_family']	   = sanitize_text_field( $caption['font_family'] );
 				$sanitized_caption['font_weight']	   = in_array( $caption['font_weight'], array( 'normal', 100, 200, 300, 400, 500, 600, 700, 800, 900 ) ) ? $caption['font_weight'] : 'normal';
