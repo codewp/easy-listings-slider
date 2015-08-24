8cc9cae188f5e2b2a433a7917f5cef14ba1ab819
taher <tah3r.a@gmail.com>
2015-08-24 18:48:01 +0430

    Using fill mode feature in introduction theme of jssor slider.

--- public/js/slider/jssor/introduction.js
+++ public/js/slider/jssor/introduction.js
@@ -114,24 +114,25 @@
 		_CaptionTransitions['FADE'] = { $Duration: 900, $Opacity: 2 };
 
 		var options = {
+			$FillMode: parseInt( data.fill_mode, 10 ),
 			$ArrowKeyNavigation: true,
 			$SlideEasing: $JssorEasing$.$EaseOutQuint,
 			$MinDragOffsetToSlide: 20,
 			$SlideSpacing: 0,
-		   $AutoPlay: '1' === data.auto_play ? true : false,           //[Optional] Whether to auto play, to enable slideshow, this option must be set to true, default value is false
-		   $AutoPlayInterval: parseInt( data.auto_play_interval ),     //[Optional] Interval (in milliseconds) to go for next slide since the previous stopped if the slider is auto playing, default value is 3000
-		   $SlideDuration: parseInt( data.slide_duration ),            //[Optional] Specifies default duration (swipe) for slide in milliseconds, default value is 500
-		   $DragOrientation: parseInt( data.drag_orientation ),        //[Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $DisplayPieces is greater than 1, or parking position is not 0)
-		   $Loop: parseInt( data.loop ),
+		    $AutoPlay: '1' === data.auto_play ? true : false,           //[Optional] Whether to auto play, to enable slideshow, this option must be set to true, default value is false
+		    $AutoPlayInterval: parseInt( data.auto_play_interval ),     //[Optional] Interval (in milliseconds) to go for next slide since the previous stopped if the slider is auto playing, default value is 3000
+		    $SlideDuration: parseInt( data.slide_duration ),            //[Optional] Specifies default duration (swipe) for slide in milliseconds, default value is 500
+		    $DragOrientation: parseInt( data.drag_orientation ),        //[Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $DisplayPieces is greater than 1, or parking position is not 0)
+		    $Loop: parseInt( data.loop ),
 
-		   $CaptionSliderOptions: {                            //[Optional] Options which specifies how to animate caption
+		    $CaptionSliderOptions: {                            //[Optional] Options which specifies how to animate caption
 		       $Class: $JssorCaptionSlider$,                   //[Required] Class to create instance to animate caption
 		       $CaptionTransitions: _CaptionTransitions,       //[Required] An array of caption transitions to play caption, see caption transition section at jssor slideshow transition builder
 		       $PlayInMode: 1,                                 //[Optional] 0 None (no play), 1 Chain (goes after main slide), 3 Chain Flatten (goes after main slide and flatten all caption animations), default value is 1
 		       $PlayOutMode: 3                                 //[Optional] 0 None (no play), 1 Chain (goes before main slide), 3 Chain Flatten (goes before main slide and flatten all caption animations), default value is 1
-		   },
+		    },
 
-		   $BulletNavigatorOptions: {                          //[Optional] Options to specify and enable navigator or not
+		    $BulletNavigatorOptions: {                          //[Optional] Options to specify and enable navigator or not
 		       $Class: $JssorBulletNavigator$,                 //[Required] Class to create navigator instance
 		       $ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
 		       $AutoCenter: 1,                                 //[Optional] Auto center navigator in parent container, 0 None, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
@@ -140,14 +141,14 @@
 		       $SpacingX: 4,                                   //[Optional] Horizontal space between each item in pixel, default value is 0
 		       $SpacingY: 4,                                   //[Optional] Vertical space between each item in pixel, default value is 0
 		       $Orientation: 1                                 //[Optional] The orientation of the navigator, 1 horizontal, 2 vertical, default value is 1
-		   },
+		    },
 
-		   $ArrowNavigatorOptions: {                           //[Optional] Options to specify and enable arrow navigator or not
+		    $ArrowNavigatorOptions: {                           //[Optional] Options to specify and enable arrow navigator or not
 		       $Class: $JssorArrowNavigator$,                  //[Requried] Class to create arrow navigator instance
 		       $ChanceToShow: 1,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
 		       $AutoCenter: 2,                                 //[Optional] Auto center arrows in parent container, 0 No, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
 		       $Steps: 1                                       //[Optional] Steps to go for each navigation request, default value is 1
-		   }
+		    }
 		};
 
 		var jssor_slider = new $JssorSlider$(data.id, options);
